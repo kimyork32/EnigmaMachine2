95eac70e3f04234e7c7e0bb19c55c0a892410342
kimyork32 <CaronteGG.20851@gmail.com>
2023-12-23 11:16:05 -0500

    un nuevo comentario en main

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 int main(int argc, char** argv){
+  cout << "hola mundo" << endl;
   if(argc < 3 || argc == 4){
     cerr << "usar: enigma plugboard-arhivo reflector-archivo rotorRotor-archivo posicionRotor-archivo" << endl;
     return NUMERO_INSUFICIENTE_DE_PARAMETROS;
@@ -28,7 +29,25 @@ int main(int argc, char** argv){
     delete enigmaDescriptacion;
     return error;
   }
-
+  int opc;
+  while(opc!=3){
+      cout << "Enigma:\n"
+          << "caso 1(1):\n"
+          << "caso 2(2)\n"
+          << "caso 3 exit(3)\n"
+          <<"opc: ";
+      cin >> opc;
+      switch(opc){
+          case 1:
+              cout << "caso 1";
+              break;
+          case 2:
+              cout << "caso 2";
+              break;
+          case 3:
+              cout << "saliendo";
+      }
+  }
   char letra;
   while(!cin.eof()){
     cin >> letra;
